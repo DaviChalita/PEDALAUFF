7b229ee32d7790b2f86ab3b4dfc9332e19d6e888
Gleison Lima <gleisonlima@id.uff.br>
2021-08-31 00:22:49 -0300

    Implantação do teste consultarUsuarioSemLogin_falha em usuarioControle

--- src/test/java/com/uff/pedalauff/controlador/UsuarioControladorTest.java
+++ src/test/java/com/uff/pedalauff/controlador/UsuarioControladorTest.java
@@ -5,19 +5,16 @@ import com.uff.pedalauff.modelo.Usuario;
 import com.uff.pedalauff.repo.UsuarioRepo;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static com.uff.pedalauff.enums.TipoUsuario.ADMIN;
-import static com.uff.pedalauff.enums.TipoUsuario.NORMAL;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -34,10 +31,29 @@ public class UsuarioControladorTest {
     @MockBean
     UsuarioRepo usuarioRepo;
 
+    @Test
+    public void consultarUsuarioSemLogin_falha() throws Exception {
+        Usuario usuario = Usuario.builder()
+                .idUsuario(3)
+                .nome("Admin")
+                .matricula(98456321)
+                .email("admin@gmail.com")
+                .senha("987456321")
+                .tipoUsuario(ADMIN)
+                .build();
+        Mockito.when(usuarioRepo.findById(usuario.getIdUsuario())).thenReturn(java.util.Optional.ofNullable(usuario));
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .post("/usuario/seusdados")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", notNullValue()))
+                .andExpect(jsonPath("$", is("Você não possui acesso para realizar tal ação.")));
+    }
 
     @Test
     public void salvarUsuario_sucesso() throws Exception {
-        Usuario usuario1 = Usuario.builder()
+        Usuario usuario = Usuario.builder()
                 .idUsuario(5)
                 .nome("test")
                 .matricula(98456321)
@@ -46,12 +62,12 @@ public class UsuarioControladorTest {
                 .tipoUsuario(ADMIN)
                 .build();
 
-        Mockito.when(usuarioRepo.save(usuario1)).thenReturn(usuario1);
+        Mockito.when(usuarioRepo.save(usuario)).thenReturn(usuario);
 
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/usuario/salvar")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .content(this.mapper.writeValueAsString(usuario1));
+                .content(this.mapper.writeValueAsString(usuario));
 
         mockMvc.perform(mockRequest)
                 .andExpect(status().isOk())
@@ -61,7 +77,7 @@ public class UsuarioControladorTest {
 
     @Test
     public void salvarUsuario_falha() throws Exception {
-        Usuario usuario1 = Usuario.builder()
+        Usuario usuario = Usuario.builder()
                 .idUsuario(3)
                 .nome("Admin")
                 .matricula(98456321)
@@ -69,12 +85,12 @@ public class UsuarioControladorTest {
                 .senha("987456321")
                 .tipoUsuario(ADMIN)
                 .build();
-        Exception e = new Exception();
+
         Mockito.when(usuarioRepo.save(ArgumentMatchers.any(Usuario.class))).thenThrow(NullPointerException.class);
         MockHttpServletRequestBuilder mockRequest = MockMvcRequestBuilders.post("/usuario/salvar")
                 .contentType(MediaType.APPLICATION_JSON)
                 .accept(MediaType.APPLICATION_JSON)
-                .content(this.mapper.writeValueAsString(usuario1));
+                .content(this.mapper.writeValueAsString(usuario));
 
         mockMvc.perform(mockRequest)
                 .andExpect(status().isOk())
