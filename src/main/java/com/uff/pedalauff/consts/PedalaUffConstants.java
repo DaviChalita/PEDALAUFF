43d514e21f4424f4513b8ecebf2a54978445cbb0
DaviChalita <chalitameneguelli14@gmail.com>
2021-09-05 18:06:43 -0300

    adiciona construtor que para impedir que a classe de constantes seja instanciada

--- src/main/java/com/uff/pedalauff/consts/PedalaUffConstants.java
+++ src/main/java/com/uff/pedalauff/consts/PedalaUffConstants.java
@@ -2,6 +2,10 @@ package com.uff.pedalauff.consts;
 
 public final class PedalaUffConstants {
 
+    private PedalaUffConstants(){
+        throw new IllegalStateException("Classe de utilidade");
+    }
+
     public static final String LOGAR_NO_SITE = "Você não possui acesso para realizar tal ação.";
     public static final StringBuilder LOGAR_NO_SITE_BUILDER = new StringBuilder(LOGAR_NO_SITE);
     public static final String ADMIN = "ADMIN";
--- src/main/java/com/uff/pedalauff/controlador/BicicletaControlador.java
+++ src/main/java/com/uff/pedalauff/controlador/BicicletaControlador.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
